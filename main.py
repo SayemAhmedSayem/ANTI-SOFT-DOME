de3469fdb9b588bce4f5d57c451c09918993b8df
SayemAhmedSayem <sayemahmed92@gmail.com>
2023-12-04 00:00:35 +0600

    ython - Global Variables

--- main.py
+++ main.py
@@ -27,5 +27,16 @@ def myfunc():
 
 myfunc()
 
+#Create a variable inside a function, with the same name as the global variable
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
 
 
