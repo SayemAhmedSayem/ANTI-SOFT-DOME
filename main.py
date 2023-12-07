9cc0f1f799658626aec5c35dedbd87a0cd6cf592
SayemAhmedSayem <sayemahmed92@gmail.com>
2023-12-08 00:07:58 +0600

    Python Data Type

--- main.py
+++ main.py
@@ -86,6 +86,22 @@ print(x)
 #display the data type of x:
 print(type(x))
 
+x = ["apple", "banana", "cherry"]
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+x = {"name" : "John", "age" : 36}
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
 
 
 
