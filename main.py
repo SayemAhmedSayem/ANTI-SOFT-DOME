9e8a1ec1aee05936db9d7b318d87de358f7632de
SayemAhmedSayem <sayemahmed92@gmail.com>
2026-01-23 22:41:51 +0600

    Update main.py

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ print(type(x))
 print(type(y))
 print(type(z))
 
-#Python Number - Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length
+Python Number - Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length
 x = 1
 y = 35656222554887711
 z = -3255522
