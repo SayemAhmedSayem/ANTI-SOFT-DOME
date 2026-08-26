3093394cd1015f2f359690d2d8b6cc83ac9a9848
Sayem Ahmed Sayem <sayemahmed92@gmail.com>
2026-08-26 23:49:52 +0600

    Update main.py

--- main.py
+++ main.py
@@ -105,12 +105,14 @@ if 5 > 2:
       print("Five is greater than two!")
 
 
-x = 5
-y = "Hello, World!"
+#x = 5
+#y = "Hello, World!"
 
 #This is a comment.
-print("Hello, World!")
+#print("Hello, World!")
 
-print("Hello World!")
-print("Have a good day.")
-print("Learning Python is fun!")
+#print("Hello World!")
+#print("Have a good day.")
+#print("Learning Python is fun!")
+
+print("Hello"); print("How are you?"); print("Bye bye!")
