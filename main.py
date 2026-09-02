bf737cf2208eaf14e12c8b22bac3c0c4658d8ff0
Sayem Ahmed Sayem <sayemahmed92@gmail.com>
2026-09-02 22:36:48 +0600

    Update main.py

--- main.py
+++ main.py
@@ -118,6 +118,9 @@ if 5 > 2:
 #print("Hello"); print("How are you?"); print("Bye bye!")
 
 #print("Hello"); print("How are you?"); print("Bye bye!")
-print(3)
-print(358)
-print(50000)
+#print(3)
+#print(358)
+#print(50000)
+
+print(3 + 3)
+print(2 * 5)
